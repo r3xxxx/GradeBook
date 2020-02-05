44b502fe2515fef1ae045848e152c1ea9229262b
Rahavee <rmanian@uvm.edu>
2020-02-04 22:08:40 -0500

    to not get confused between grade student and students grade

--- Parse.py
+++ Parse.py
@@ -9,14 +9,17 @@ userInput=input("Type in a command: ")
 #check for matches with any existing command
 #the value for this function can be taken out while parsing
 def errorParsing(userInput, command, value):
-    setCommand="["+command+"]" #to make sure that the findall searches for chars and not the keyword
-    matches = re.findall(setCommand, userInput)
-    print(matches)
-    if len(matches) > len(command)/4:
-        correctCommand = "Did you mean "+command +" "+ value + " ?(y/n): "
-        check=input(correctCommand)
-        if check=='y':
-            return True
+    userInput=userInput[0:len(command)]
+    if userInput[0]==command[0]: # to not get GRADE STUDENT AND STUDENTS GRADE confused
+        setCommand="["+command+"]"
+        print(setCommand)#to make sure that the findall searches for chars and not the keyword
+        matches = re.findall(setCommand, userInput)
+        print(matches)
+        if len(matches) > len(command)/4:
+            correctCommand = "Did you mean "+command +" "+ value + " ?(y/n): "
+            check=input(correctCommand)
+            if check=='y':
+                return True
     return False
 
-errorParsing(userInput, " STUDENTS GRADE", "RAHAVEE")
+errorParsing(userInput, "STUDENTS GRADE", "RAHAVEE")
